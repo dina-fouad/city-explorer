930560761ee75baad6abac82d2405f6e67a0b86b
dina-fouad <dina_albarghouthi@yahoo.com>
2021-06-13 17:02:45 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,45 +7,42 @@ class App extends React.Component{
     this.state ={
       Data:'',
       errMsg:'',
-      displayErrMsg: false,
-      displayMap:false
+      errorMsg: false,
+     Map:false
     }
   }
   getLocation = async(event) =>{
     event.preventDefault();
     let seachQuery = event.target.searchQuery.value;
-    let locURL = `https://us1.locationiq.com/v1/search.php?key=pk.b5c1fd2f37c40bd3df773b1b16f2cf35&q=${seachQuery}&format=json`;
+    let locationURL = `https://us1.locationiq.com/v1/search.php?key=pk.b5c1fd2f37c40bd3df773b1b16f2cf35&q=${seachQuery}&format=json`;
     try {
-      let locResult = await axios.get(locURL);
-      console.log(locResult.data);
+      let locationResult = await axios.get(locationURL);
       this.setState({
-        Data:locResult.data[0],
-        displayMap:true
+        Data:locationResult.data[0],
+        Map:true
       })
     }
     catch {
       this.setState({
         errMsg: 'Unable to geocode',
-        displayErrMsg:true
+        errorMsg:true
       })
     }
-    
   }
 
   render(){
     return(
       <div>
-        <h1>React axios app</h1>
-        {/* <button onClick={this.getLocation}>search</button> */}
+        <h2>choose your location</h2>
         <form onSubmit={this.getLocation}>
-          <input type='text' placeholder='city name' name='searchQuery' />
+          <input type='text' placeholder='location name' name='searchQuery' />
           <input type='submit' value='explore' />
         </form>
         <p>{this.state.Data.display_name}</p>
         <p>{this.state.Data.lon}</p>
         <p>{this.state.Data.lat}</p>
-        {this.state.displayMap &&  <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.b5c1fd2f37c40bd3df773b1b16f2cf35&center=${this.state.Data.lat},${this.state.Data.lon}`} alt='map'/> }
-        { this.state.displayErrMsg && this.state.errMsg}
+        {this.state.Map &&  <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.b5c1fd2f37c40bd3df773b1b16f2cf35&center=${this.state.Data.lat},${this.state.Data.lon}`} alt='map'/> }
+        { this.state.errorMsg && this.state.errMsg}
       </div>
     )
   }
